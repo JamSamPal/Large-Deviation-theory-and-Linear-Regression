93643b22f982f715ad2fee0801505f7f70bd303e
JamSamPal <141417392+JamSamPal@users.noreply.github.com>
2024-01-03 14:31:54 +0000

    Update Large deviations and Linear Regression.py

--- Large deviations and Linear Regression.py
+++ Large deviations and Linear Regression.py
@@ -19,13 +19,14 @@ function of the estimation parameters of the linear fit - err_i = y_i- (b_0 + b_
 As log is an increasing function we can also just maximise P using gradient descent. 
 """
 import numpy as np
+import matplotlib.pyplot as plt 
 
 #initialising
-time = 1000 #number of iterations
-learning_rate =0.1
+time = 100000 #number of iterations
+learning_rate =0.0002
 x = np.array([5, 15, 25, 35, 45, 55]) #inputs
 y = np.array([5, 20, 14, 32, 22, 38]) #outputs
-theta_0 = [0,0] #b_0, b_1
+theta_0 = [0.2,0.1] #b_0, b_1
 
 #Gradient descent algorithm
 def gradient_descent(learning_rate, theta_0, time, gradient, x, y):
@@ -34,17 +35,41 @@ def gradient_descent(learning_rate, theta_0, time, gradient, x, y):
         theta_new += -learning_rate*np.array(gradient(x,y,theta_new))
     return theta_new
 
-
-#Maximum likelihood for P(err_i) = err_i log(err_i) + (1- err_i) log(1- err_i)
-def grad_1(x,y, theta):
-    derivative = np.log(np.abs((y- theta[0] - theta[1]*x)/(1-(y- theta[0] - theta[1]*x))))
+#Gaussian least squares
+def grad(x,y,theta):
+    derivative =theta[0] + theta[1]*x-y
     return derivative.mean(), (derivative*x).mean()
 
 
-print(gradient_descent(learning_rate, theta_0, time, grad_1, x, y))
+#Maximum likelihood for P(err_i) = err_i log(err_i) + (1- err_i) log(1- err_i)
+def grad_1(x,y, theta):
+    derivative = -np.log(np.abs((y- theta[0] - theta[1]*x)/(1-(y- theta[0] - theta[1]*x))))
+    return derivative.mean(), (derivative*x).mean()
 
-    
-    
+def P_err_1(x,y, theta):
+    func = (y- theta[0] - theta[1]*x)*np.log(np.abs((y- theta[0] - theta[1]*x))) + (1-(y- theta[0] - theta[1]*x))*np.log(np.abs(1-(y- theta[0] - theta[1]*x)))
+    return func.mean()
+
+Len =301
+beta_0 = np.linspace(-5, 5, Len)
+beta_1 = np.linspace(-5, 5, Len)
+h = np.zeros((Len,Len))
+for i,m in enumerate(beta_0):
+    for j,n in enumerate(beta_1):
+        h[i][j] = P_err_1(x,y,(m,n))
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.plot_surface(beta_0, beta_1, h)
+#plt.colorbar()
+plt.xlabel('b_0')
+plt.ylabel('b_1')
+plt.show()
+
+print(gradient_descent(learning_rate, theta_0, time, grad, x, y))
+print(gradient_descent(learning_rate, theta_0, time, grad_1, x, y))
     
     
 
